b7c87256100eaddb5da76fa5fd53693e9a0875b6
Lissandre <lissandre.pasdeloup@hetic.net>
2020-09-24 15:31:22 +0200

    animations

--- src/js/World/Dolphins.js
+++ src/js/World/Dolphins.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { gsap, TweenMax} from 'gsap'
 
 import DolphinsTexture from '../../textures/low-poly-dolphin/out.glb'
 
@@ -13,17 +14,40 @@ export default class Dolphins{
     this.container = new THREE.Object3D()
     this.container.matrixAutoUpdate = false
     this.loader = new GLTFLoader()
-
+    this.clock = new THREE.Clock()
 
     this.setDolphins()
   }
   setDolphins(){
     this.loader.load(DolphinsTexture, (model) => {
-      model.scene.position.set(100, -400, -150)
+      model.scene.position.set(400, -400, -150)
       model.scene.scale.set(0.3,0.3,0.3)
-      model.scene.rotateY(2*Math.PI/3)
-      model.scene.rotateX(-Math.PI/5)
-      this.container.add(model.scene)
+      model.scene.rotateY(Math.PI/2)
+      this.scene = model.scene
+      // model.scene.rotateX(-Math.PI/5)
+      this.mixer = new THREE.AnimationMixer( this.scene )
+      this.action = this.mixer.clipAction( model.animations[ 0 ] ) // play the first animation
+      this.action.play()
+      console.log(model)
+      this.container.add(this.scene)
+      this.setDolphinsMovement()
     })
+  }
+  setDolphinsMovement(){
+    this.time.on('tick', ()=>{
+      this.delta = this.clock.getDelta()
+      this.mixer.update( this.delta ) 
+      if(this.scene.position.x > 400 || this.scene.position.x < -400){
+        this.container.remove(this.scene)
+        this.setDolphins()
+      }
+    })
+    setInterval(()=>{
+      TweenMax.to(this.scene.position, {
+        duration: 2.66,
+        delay: 0.1,
+        x: this.scene.position.x - 80,
+      })
+    }, 2700)
   }
 }
--- src/js/World/Whale.js
+++ src/js/World/Whale.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { gsap, TweenMax } from 'gsap'
 
 import WhaleTexture from '../../textures/Whale.fbx'
 
@@ -13,20 +14,39 @@ export default class Whale{
     this.container = new THREE.Object3D()
     this.container.matrixAutoUpdate = false
     this.loader = new FBXLoader()
+    this.clock = new THREE.Clock()
 
     this.setWhale()
   }
   setWhale(){
     this.loader.load(WhaleTexture, (object) => {
       //object.children[1].scale.set(0.0005,,0.0005)
-      object.position.set(
-        -50,
+      this.scene = object
+      this.scene.position.set(
+        -400,
         -1100,
         -150)
-      this.container.add(object)
-      object.scale.set(0.2, 0.2, 0.2)
-      object.rotateY(Math.PI/2)
-      
+      this.container.add(this.scene)
+      this.scene.scale.set(0.2, 0.2, 0.2)
+      this.scene.rotateY(Math.PI/2)
+      this.mixer = new THREE.AnimationMixer( this.scene )
+      this.action = this.mixer.clipAction( this.scene.animations[ 1 ] ) // play the first animation
+      this.action.play()
+      this.setAnimationWhale()
+    })
+  }
+  setAnimationWhale(){
+    this.time.on('tick', ()=>{
+      this.delta = this.clock.getDelta()
+      this.mixer.update( this.delta ) 
+      TweenMax.to(this.scene.position, {
+        duration: 0.05,
+        x: this.scene.position.x + 0.3,
+      })
+      if(this.scene.position.x > 400 || this.scene.position.x < -400){
+        this.container.remove(this.scene)
+        this.setWhale()
+      }
     })
   }
 }
